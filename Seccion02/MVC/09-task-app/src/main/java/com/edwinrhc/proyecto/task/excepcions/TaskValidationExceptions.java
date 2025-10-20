666ec671543746530131ed9f15578575b1b3c37a
Edwin HC <edwinrhc@gmail.com>
2025-10-20 18:33:33 -0500

    feat(TaskValidationException):  change TaskException from RuntimeException to checked Exception for explicit error handling

---
+++ Seccion02/MVC/09-task-app/src/main/java/com/edwinrhc/proyecto/task/excepcions/TaskValidationExceptions.java
@@ -0,0 +1,7 @@
+package com.edwinrhc.proyecto.task.excepcions;
+
+public class TaskValidationExceptions extends Exception {
+    public TaskValidationExceptions(String message) {
+        super(message);
+    }
+}
