a39e041ebd6affc130102fe550eeaa1ff3230692
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-10 03:45:36 -0500

    git commit -m "feat(datastore): implementar clase genérica, agregar metodos add,remove, find y getAll(). Agregar Main

--- Seccion02/MVC/09-task-app/src/main/java/com/edwinrhc/proyecto/task/Main.java
+++ Seccion02/MVC/09-task-app/src/main/java/com/edwinrhc/proyecto/task/Main.java
@@ -1,11 +1,22 @@
 package com.edwinrhc.proyecto.task;
 
+import com.edwinrhc.proyecto.task.controller.TaskController;
+import com.edwinrhc.proyecto.task.model.TaskRepository;
+import com.edwinrhc.proyecto.task.view.TaskView;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+
+        TaskRepository repository = new TaskRepository();
+        TaskController controller = new TaskController(repository);
+        TaskView view = new TaskView(controller);
+
+        view.showMenu();
+
 
     }
 }
--- Seccion02/MVC/09-task-app/src/main/java/com/edwinrhc/proyecto/task/view/TaskView.java
+++ Seccion02/MVC/09-task-app/src/main/java/com/edwinrhc/proyecto/task/view/TaskView.java
@@ -3,7 +3,9 @@ package com.edwinrhc.proyecto.task.view;
 import com.edwinrhc.proyecto.task.controller.TaskController;
 import com.edwinrhc.proyecto.task.excepcions.TaskException;
 import com.edwinrhc.proyecto.task.excepcions.TaskValidationExceptions;
+import com.edwinrhc.proyecto.task.model.Task;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TaskView {
@@ -11,9 +13,9 @@ public class TaskView {
     private final TaskController taskController;
     private final Scanner scanner;
 
-    public TaskView(TaskController taskController, Scanner scanner) {
+    public TaskView(TaskController taskController) {
         this.taskController = taskController;
-        this.scanner = scanner;
+        this.scanner = new Scanner(System.in);
     }
 
     public void showMenu() {
@@ -30,19 +32,20 @@ public class TaskView {
                 case "1":
                     addTaskView();
                     break;
-
                 case "2":
-
+                    removeTaskView();
                     break;
                 case "3":
-
+                    updateTaskView();
                     break;
                 case "4":
-
+                    showTaskView();
                     break;
                 case "5":
-
-                    break;
+                    System.out.println("Saliendo del sistema");
+                    return;
+                default:
+                    System.out.println("Opción inválida, intente de nuevo");
 
             }
         }
@@ -50,25 +53,94 @@ public class TaskView {
 
     public void addTaskView() {
         try {
-            System.out.println("Ingresar ID");
-            String id = scanner.nextLine();
 
-            System.out.println("Ingrese el Título");
-            String title = scanner.nextLine();
-
-            System.out.println("Ingrese la descripción");
-            String description = scanner.nextLine();
-
-            System.out.println("¿Esá completada? true/false");
-            Boolean completed = Boolean.valueOf(scanner.nextLine());
-
-            taskController.addTask(id, title, description, completed);
+            Task task = getTaskInput();
+            taskController.addTask(task.getId(), task.getTitle(), task.getDescription(), task.getCompleted());
             System.out.println("La tarea fue agregada exitosamente");
-        }catch (TaskException e) {
+        } catch (TaskException e) {
             System.out.println("Error: " + e.getMessage());
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error inesperado, Contacte con el soporte" + e.getMessage());
             e.printStackTrace();
         }
     }
+
+    public void removeTaskView() {
+        try {
+            System.out.println("Ingrese el ID de la tarea a eliminar");
+            String id = scanner.nextLine();
+            this.taskController.removeTask(id);
+            System.out.println("Tarea eliminada exitosamente");
+        } catch (TaskValidationExceptions | TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado, Contacte con el soporte" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void showTaskView() {
+        try {
+            System.out.println("\nLa lista de Tareas");
+            this.taskController.showTaks();
+
+        } catch (TaskValidationExceptions | TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado, Contacte con el soporte" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void updateTaskView() {
+        try {
+
+            Task task = getTaskInput();
+            taskController.updateTask(task.getId(), task.getTitle(), task.getDescription(), task.getCompleted());
+            System.out.println("La tarea fue actualizada exitosamente");
+        } catch (TaskException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado, Contacte con el soporte" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private Task getTaskInput() {
+
+
+       String id = readRequiredString("ID de la tarea");
+       String title = readRequiredString("titulo de la tarea");
+       String description = readRequiredString("descripción de la tarea");
+       Boolean completed = readBoolean("¿Esá completada? true/false");
+        return new Task(id, title, description, completed);
+    }
+
+    private String readRequiredString(String message) {
+        String input;
+        do {
+            System.out.println("Ingrese " + message + ":");
+            input = scanner.nextLine().trim();
+
+            if (input.isEmpty()) {
+                System.out.println("El campo '" + message + "' no puede estar vacío");
+            }
+
+        } while (input.isEmpty());
+
+        return input;
+    }
+    private Boolean readBoolean(String message){
+        String input;
+        do{
+            System.out.println("Ingrese " + message + ":");
+            input = scanner.nextLine().trim().toLowerCase();
+
+            if(!input.equals("true") && !input.equals("false")){
+                System.out.println("Ingrese solo 'true' o 'false'");
+            }
+        }while (!input.equals("true") && !input.equals("false"));
+        return Boolean.parseBoolean(input);
+    }
+
 }
